b51fe86cd58c7eb86e92f2283bb165668af4568f
SimonKempf <sim.kempf@gmail.com>
2018-02-16 22:09:12 +0100

    ajout de variables dans les deux fichiers

--- fichier1.js
+++ fichier1.js
@@ -1,2 +1,4 @@
 console.log('Hello, je suis le fichier number one')
 console.log('première modification du fichier')
+var x = 1;
+var y = 2;
--- fichier2.js
+++ fichier2.js
@@ -1,1 +1,3 @@
 console.log('hey je suis le fichier number 2')
+var y = 56;
+var w = 67;
